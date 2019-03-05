e59caa515fc590b6a1a341658e1d3284a0925d27
MurrayCC-home <330784960@qq.com>
2019-03-05 21:49:51 +0800

    add video2frame util

---
+++ video2frame.py
@@ -0,0 +1,36 @@
+import os
+import cv2
+
+videos_src_path = '../DeepStab/stable'
+videos_save_path = './data_video/stable'
+
+videos = os.listdir(videos_src_path)
+videos = filter(lambda x: x.endswith('avi'), videos)
+
+for each_video in videos:
+    print(each_video)
+
+    # get the name of each video, and make the directory to save frames
+    each_video_name, _ = each_video.split('.')
+    os.mkdir(videos_save_path + '/' + each_video_name)
+
+    each_video_save_full_path = os.path.join(videos_save_path, each_video_name) + '/'
+
+    # get the full path of each video, which will open the video tp extract frames
+    each_video_full_path = os.path.join(videos_src_path, each_video)
+
+    cap = cv2.VideoCapture(each_video_full_path)
+    frame_count = 1
+    success = True
+    while (success):
+        success, frame = cap.read()
+        print('Read a new frame: ', success)
+
+        params = []
+        params.append(cv2.IMWRITE_PXM_BINARY)
+        params.append(1)
+        cv2.imwrite(each_video_save_full_path + each_video_name + "%d.jpg" % frame_count, frame, params)
+
+        frame_count = frame_count + 1
+
+cap.release()
